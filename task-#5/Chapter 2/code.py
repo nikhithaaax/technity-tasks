f59239831af4f6630deff7ecb3929f1ebbdf07af
nikhitha P <113986506+nikhithaaax@users.noreply.github.com>
2023-08-07 18:34:30 +0530

    Create code.py

---
+++ task-#5/Chapter 2/code.py
@@ -0,0 +1,44 @@
+#3rd
+
+print("truth table of and operator")
+print("False and True gives", (True and True))
+print("True and False gives", (True and False))
+print("True and True gives", (False and True))
+print("False and False gives", (False and False), "\n")
+
+print("truth table of or operator")
+print("False and True gives", (True or True))
+print("True and False gives", (True or False))
+print("True and True gives", (False or True))
+print("False and False gives", (False or False), "\n")
+
+print("truth table of not operator")
+print("not True gives", (not True))
+print("not False gives",  (not False))
+
+
+#9th 
+spam=input('enter a number : ')
+if spam=="1":
+    print("Hello")
+elif spam=="2":
+    print("Howdy")
+else:
+    print("Greetings!")
+
+
+#13th 
+#using for loop 
+i=0
+for i in range(10):
+    print(i+1)
+#using while loop
+i=0
+while i<10:
+    print(i+1)
+    i=i+1
+
+
+
+
+
