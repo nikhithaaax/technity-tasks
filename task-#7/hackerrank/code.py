d71fd53a7f735b2e85d3366027f105926b376349
nikhitha P <113986506+nikhithaaax@users.noreply.github.com>
2023-08-22 19:28:24 +0530

    Update code.py

--- task-#7/hackerrank/code.py
+++ task-#7/hackerrank/code.py
@@ -16,3 +16,26 @@ alice, bob = compareTriplets(alice_triplet, bob_triplet)
 
 print(f"score: ({alice},{bob})")
 
+#2
+
+def diagonal_difference(arr):
+    n = len(arr)
+    d1 = 0
+    d2 = 0
+
+    for i in range(n):
+        d1 += arr[i][i]
+        d2 += arr[i][n - i - 1]
+
+    return abs(d1 - d2)
+
+
+arr = [
+    [11, 2, 4],
+    [4, 5, 6],
+    [10, 8, -12]
+]
+
+result = diagonal_difference(arr)
+print("Diagonal difference:", result)
+
