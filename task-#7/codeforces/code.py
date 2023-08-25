92a31b9f4c79927ea360e96abd849190c3cabefc
nikhitha P <113986506+nikhithaaax@users.noreply.github.com>
2023-08-25 15:43:30 +0530

    Update code.py

--- task-#7/codeforces/code.py
+++ task-#7/codeforces/code.py
@@ -14,3 +14,15 @@ n, m, a = map(int, input("Enter n, m, and a: ").split())
 
 result = theatresquare(n, m, a)
 print("Number of flagstones required:", result)
+
+#2
+
+word=input("Enter a word : ")
+def abbrevated_word(word):
+	if len(word) > 10:
+		return word[0] + str(len(word) - 2) + word[-1]
+	else: 
+		return word
+		
+result=abbrevated_word(word)
+print("Sorted word is : ",result )
