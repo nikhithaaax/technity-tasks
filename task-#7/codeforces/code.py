a9cd17e16fe0305b92282b879ae84c8f4ee112ff
nikhitha P <113986506+nikhithaaax@users.noreply.github.com>
2023-08-26 02:17:32 +0530

    Update code.py

--- task-#7/codeforces/code.py
+++ task-#7/codeforces/code.py
@@ -26,3 +26,78 @@ def abbrevated_word(word):
 		
 result=abbrevated_word(word)
 print("Sorted word is : ",result )
+
+
+#3
+
+def main():
+    n, k = map(int, input().split())
+    scores = list(map(int, input().split()))
+
+    kscore = scores[k - 1]  
+
+    
+    count = 0
+    for score in scores:
+        if score >= kscore and score > 0:
+            count = count+1
+
+    print(count)
+
+if __name__ == "__main__":
+    main()
+
+
+#4
+
+def main():
+    n = int(input("Enter no. of operations : "))  
+    x = 0  
+
+    for _ in range(n):
+        operation = input("Enter operation/s : ")
+        if "++" in operation:
+            x = x+1
+        else:
+            x = x-1
+
+    print("Result : ",x)
+
+if __name__ == "__main__":
+    main()
+
+
+#5
+
+def main():
+    team=input()
+    
+    consecutivezeros=0
+    consecutiveones=0
+    is_dangerous=False
+    
+    for char in team:
+        if char=="1":
+            consecutiveones += 1 
+            consecutivezeros = 0
+        else:
+            consecutivezeros += 1 
+            consecutiveones = 0
+        if consecutiveones >=7 or consecutivezeros >=7:
+            is_dangerous=True
+            break
+    
+    
+    if is_dangerous:
+        print("YES")
+        
+    else:
+        print("NO")
+    
+            
+
+if __name__ == "__main__":
+    main()  
+    
+
+
